142344e8a8266017c6828c69a864512212802c3b
Joseph Rignanese <rignanes@adobe.com>
2023-03-17 18:45:30 +1100

    #6 fixed lint issues (again)

--- blocks/article-list/article-list.js
+++ blocks/article-list/article-list.js
@@ -1,14 +1,14 @@
 async function getArticles(block) {
-  fetch("/article-index.json")
+  fetch('/article-index.json')
     .then((response) => response.json())
     .then((articles) => {
-      const list = document.createElement("ul");
+      const list = document.createElement('ul');
       const fragment = document.createDocumentFragment();
 
       articles.data.forEach((result) => {
-        const li = document.createElement("li");
-        const a = document.createElement("a");
-        a.setAttribute("href", result.path);
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.setAttribute('href', result.path);
         a.textContent = result.title;
         li.append(a);
         fragment.append(li);
