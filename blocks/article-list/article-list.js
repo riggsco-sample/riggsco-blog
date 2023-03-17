389ee42702563fcf394d157141602dfd24e46596
Joseph Rignanese <rignanes@adobe.com>
2023-03-17 18:26:37 +1100

    #6 Added article list block

---
+++ blocks/article-list/article-list.js
@@ -0,0 +1,24 @@
+export default function decorate(block) {
+  getArticles(block);
+}
+
+async function getArticles(block) {
+  fetch("/article-index.json")
+    .then((response) => response.json())
+    .then((articles) => {
+      const list = document.createElement("ul");
+      const fragment = document.createDocumentFragment();
+
+      articles.data.forEach((result) => {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.setAttribute("href", result.path);
+        a.textContent = result.title;
+        li.append(a);
+        fragment.append(li);
+      });
+
+      list.append(fragment);
+      block.append(list);
+    });
+}
